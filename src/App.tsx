fc03f5fd3747dc2ded8e8bfc02d9268aaa555539
Joanderson Silva <jhonny_040996@hotmail.com>
2023-01-27 00:05:44 -0300

    feat: configuração de tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
-import { defaultTheme } from "./styles/theme/default";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-    </ThemeProvider>
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <Button variant="primary" />
+        <Button variant="secondary" />
+        <Button variant="danger" />
+        <Button variant="success" />
+        <Button />
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
   );
 }
