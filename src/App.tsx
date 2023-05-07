b369bd548d383a1af0bb38c798ac607752e3214e
Joanderson Silva <jhonny_040996@hotmail.com>
2023-05-06 22:49:04 -0300

    feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
-import { GlobalStyle } from "./styles/global";
-import { defaultTheme } from "./styles/themes/default";
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './routes/Routes'
 
 export function App() {
   return (
     <>
       <ThemeProvider theme={defaultTheme}>
-        <Button variant="primary" />
-        <Button variant="secondary" />
-        <Button variant="danger" />
-        <Button variant="success" />
-        <Button />
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
         <GlobalStyle />
       </ThemeProvider>
     </>
-  );
+  )
 }
